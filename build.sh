af764dcf60a4e98ba24a2c3e00734696e731920c
dylanetaft <dylanetaft@dylanetaft-imac.local>
2016-07-24 11:15:39 -0400

    Test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+#test
 clang++ `pkg-config --libs gnutls --cflags` `pkg-config --libs protobuf --cflags` -ggdb  -std=c++1z test.cpp ./libmumbot/MumBotState.cpp ./libmumbot/MumBotConnectionMgr.cpp ./libmumbot/proto/Mumble.pb.cc -o test
